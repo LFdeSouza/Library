08a26ce69208801b6e71d392bfa74c61e1afb42b
LFS1245 <luiz.f.souza@live.com>
2021-09-20 15:13:41 -0300

    add to storage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// define book
 function Book(title, author, pageNum, readStatus) {
   this.title = title;
   this.author = author;
@@ -8,27 +9,49 @@ function Book(title, author, pageNum, readStatus) {
       : '<ion-icon class="not-read-icon" name="close"></ion-icon>';
 }
 
-function addBook(newBook) {
-  const bookList = document.querySelector("[data-book-list]");
+//Storage handling
+let library = !localStorage.books ? [] : JSON.parse(localStorage.books);
+
+// Load books upon loading
+window.addEventListener("load", (event) => {
+  library.forEach((book) => {
+    displayBooks(book);
+  });
+});
+
+function addBookToStorage(newBook) {
+  let storedBooks = !localStorage.books ? [] : JSON.parse(localStorage.books);
+  storedBooks.push(newBook);
+  localStorage.books = JSON.stringify(storedBooks);
+}
+
+function removeBookFromStorage(book) {
+  // let storedBooks = JSON.parse(localStorage.books);
+  // storedBooks.push(newBook);
+  // localStorage.books = JSON.stringify(storedBooks);
+}
+
+function displayBooks(book) {
+  const bookList = document.querySelector("#book-list");
 
   const bookRow = document.createElement("tr");
 
-  bookRow.innerHTML = `<th>${newBook.title}</th>
-                       <td>${newBook.author}</td>
-                       <td>${newBook.pageNum}</td>
-                       <td>${newBook.readStatus}</td>
+  bookRow.innerHTML = `<th>${book.title}</th>
+                       <td>${book.author}</td>
+                       <td>${book.pageNum}</td>
+                       <td>${book.readStatus}</td>
                        <td><ion-icon class="delete-icon" name="trash"></ion-icon></td>`;
 
   bookList.appendChild(bookRow);
 }
 
-//create new book upon submition
+//Event: create new book upon submition
 const form = document.querySelector(".entry-fields");
 form.addEventListener("submit", (e) => {
   //prevent default submit behavior
   e.preventDefault();
 
-  //get data and create book
+  // Get data and create book
   const title = document.querySelector("#book-title").value;
   const author = document.querySelector("#book-author").value;
   const pages = document.querySelector("#book-pages").value;
@@ -36,11 +59,28 @@ form.addEventListener("submit", (e) => {
     ? "read"
     : "unread";
 
-  console.log(title, author, pages, readStatus);
   const newBook = new Book(title, author, pages, readStatus);
-  console.log(newBook);
-  addBook(newBook);
+  addBookToStorage(newBook);
+  displayBooks(library);
+  clearFields();
 });
 
-const book1 = Book("Eye of the world", "Robert Jordan", 832, "read");
-window.localStorage.setItem(1, book1);
+function clearFields() {
+  document.querySelector("#book-title").value = "";
+  document.querySelector("#book-author").value = "";
+  document.querySelector("#book-pages").value = "";
+  document.querySelector("#checkbox").checked = false;
+}
+
+// Event: Remove Book
+const removeButton = document.querySelectorAll("#book-list");
+removeButton.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    //Remove from list
+    if (e.target.classList.contains("delete-icon")) {
+      console.log(e.target);
+      e.target.parentElement.parentElement.remove();
+      // Remove from storage
+    }
+  })
+);
